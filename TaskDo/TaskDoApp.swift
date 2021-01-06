b157a67231c58842085900e4caed20fd5bd258d5
Yong Liang Herr <philippeyong927@gmail.com>
2021-01-06 14:20:41 +0800

    Initial Commit

---
+++ TaskDo/TaskDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDoApp.swift
+//  TaskDo
+//
+//  Created by Philippe Yong on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TaskDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
